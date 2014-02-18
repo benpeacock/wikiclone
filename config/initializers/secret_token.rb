34a108906d7f27b4f86926ab6b4a86a5165c7cd9
Ben <ben9910@gmail.com>
2014-02-18 02:53:02 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikiclone::Application.config.secret_token = '645f77c21bb72b98c072c1f4340a28401f981fdc6be7f28f6503af59700fcd818a3ef3a89596d6ec160379f198fdcdb0d9173f3d1a81fc47f8bf2a2dee9476db'
